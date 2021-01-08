be482d7447911339805e03fef6fae2e8a92d5b8b
Ahmadul Karim Chowdhury <ahmadulkc@gmail.com>
2021-01-08 23:55:40 +0600

    Add files via upload

---
+++ Data Structure and Algorithm/Binary_Search_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int array[], int x, int low, int high)
+{
+    // Repeat until the pointers low and high meet each other
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (array[mid] == x)
+            return mid;
+        if (array[mid] < x)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main(void)
+{
+    int array[] = {1, 4, 7, 9, 11, 17, 20};
+    int x = 10;
+    int n = sizeof(array) / sizeof(array[0]);
+    int result = binarySearch(array, x, 0, n - 1);
+    if (result == -1)
+        printf("Not found");
+    else
+        printf("Element is found at index %d", result);
+}
